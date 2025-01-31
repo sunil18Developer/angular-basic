cba470ef4aecb2cecdf0324a86a1e9b42dd63d08
Sunil Otale <sunilotale181298@gmail.com>
2025-01-31 11:28:28 +0530

    for loop example is done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wish-item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-basic';
+  wishItems = [
+    new WishItem('Learn Angular', false),
+    new WishItem('Build a website', false),
+    new WishItem('Travel the world', false)
+  ];
+  title = 'Wish List';
 }
